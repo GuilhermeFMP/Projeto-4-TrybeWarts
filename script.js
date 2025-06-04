aacd5d0904a87f0b2947ceb92c603072f8317d1a
Yasmin Pompe <yasminpompe99@gmail.com>
2025-06-04 19:59:06 -0300

    ajeitei o css

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const loginBtn = document.getElementById('login-btn')
+const loginBtn = document.getElementById('login-btn');
+const emailInput = document.querySelector('.trybewarts-login input[name="email"]');
+const passwordInput = document.querySelector('.trybewarts-login input[name="password"]');
 
-loginBtn.addEventListener('click', () => {
-    const email = document.querySelector('input[name="email"]')
-    const password = document.querySelector('input[name = "password"]')
+loginBtn.addEventListener('click', function(event){
+    event.preventDefault()
 
-    //estritamente igual? 
-    //tá dando errado essa bomba
-    if(email === 'trybe@teste.com' && password === '123456'){
-        alert('Olá, Tryber')
+    const email = emailInput
+    const password = passwordInput
+
+    if(email === 'tryber@teste.com' && password === '123456'){
+        alert('Olá, Tryber!')
     } else{
-        alert('Email ou senha inválidos')
+        alert('Email ou senha inválidos.')
     }
+})
+
+const checkbox = document.getElementById('agreement')
+const button = document.getElementById('submit-btn')
+
+checkbox.addEventListener('change', function (){
+    button.disabled = !checkbox.checked
 })
